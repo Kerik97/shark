8dfc25a9a858ab5e2caca2fdeff2995cc6854d9b
kirill <k.grinikh@gmail.com>
2022-08-13 09:26:23 +0300

    Найти кубы чисел от 1 до N

---
+++ task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти кубы чисел от 1 до N
+
+int i = 1;
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine("Число" + "\t" + "Куб");
+for (i = 1; i <= N; i++)
+{
+    Console.WriteLine(i + "\t" + i * i * i);
+}
